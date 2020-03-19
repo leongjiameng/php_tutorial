42cb76c9f23404c5d6968258bcc7b3b446dfde78
Leong Jia Meng <leongjiameng@outlook.com>
2020-03-19 18:54:24 +0800

    First Commit

---
+++ index.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Document</title>
+    <style>
+        header{
+            background:#e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+  <ul>
+        <?php
+            foreach($names as $name){
+                echo"<li>$name</li>";
+            }
+        ?>
+
+        <!-- 
+        <?php foreach ($names as $name): ?>
+            <li><?= $name; ?></li>
+        <?php endforeach; ?>
+        -->
+
+  </ul>
+
+
+
+
+
+
+</body>
+</html>
